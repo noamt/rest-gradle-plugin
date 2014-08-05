3530efbafe5841aecc69a31b21ce7e317e0078b3
Andrea Rossi <andrea.rossi@here.com>
2014-08-05 23:04:47 +0200

    Refactoring RestTask
    The server response now is saved inside an instance field, so that
    child tasks can use it if needed
    Now the validation of the input is done inside a dedicated method
    Adds an additional check to assure that if you do a GET request you don't
    set a request body

--- src/main/groovy/org/_10ne/gradle/rest/RestTask.groovy
+++ src/main/groovy/org/_10ne/gradle/rest/RestTask.groovy
@@ -15,7 +15,6 @@
  */
 
 package org._10ne.gradle.rest
-
 import groovyx.net.http.HttpResponseDecorator
 import groovyx.net.http.RESTClient
 import org.apache.commons.lang.StringUtils
@@ -25,7 +24,6 @@ import org.gradle.api.InvalidUserDataException
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.Optional
 import org.gradle.api.tasks.TaskAction
-
 /**
  * @author Noam Y. Tenne
  */
@@ -49,6 +47,8 @@ class RestTask extends DefaultTask {
     @Optional Object requestBody
     @Input
     @Optional Object requestHeaders
+
+    HttpResponseDecorator serverResponse
 
     RestTask() {
         httpMethod = 'get'
@@ -98,8 +98,6 @@ class RestTask extends DefaultTask {
 
         println "Executing a '$httpMethod' request to '$uri'"
 
-        HttpResponseDecorator responseDecorator = client."${httpMethod.toLowerCase()}"(params)
-
-        println "Received response: ${responseDecorator.getData()}"
+        serverResponse = client."${httpMethod.toLowerCase()}"(params)
     }
 }
